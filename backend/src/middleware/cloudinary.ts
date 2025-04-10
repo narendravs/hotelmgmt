05fdf19e2b09acee6afa191cd93d69c5be83c937
narendravs <narendravs_mca@yahoo.com>
2025-04-10 17:31:06 +0530

    configured cloudinary to store and fetch uploaded imgaes

---
+++ backend/src/middleware/cloudinary.ts
@@ -0,0 +1,8 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dcd41ekwu",
+  api_key: "947198711632983",
+  api_secret: "C7ltooqxJYjmuQFB0kYGJVjTi3c",
+});
+module.exports = cloudinary;
