721db6083498a914e5d6d6e64b04ffee1ce0a7f0
narendravs <narendravs_mca@yahoo.com>
2025-04-10 20:16:20 +0530

    added the routes for login and register pages

--- backend/dist/src/middleware/cloudinary.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const cloudinary = require("cloudinary").v2;
-cloudinary.config({
-    cloud_name: "dcd41ekwu",
-    api_key: "947198711632983",
-    api_secret: "C7ltooqxJYjmuQFB0kYGJVjTi3c",
-});
-module.exports = cloudinary;
