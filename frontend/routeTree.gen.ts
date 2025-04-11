9adb6bc4d478dd0ea7da910012c4ec2c55405938
narendravs <narendravs_mca@yahoo.com>
2025-04-11 11:55:48 +0530

    configured the files as per required to implement hotelmgmt project

---
+++ frontend/routeTree.gen.ts
@@ -0,0 +1,18 @@
+import { Route as rootRoute } from "./src/routes/_root";
+
+// import { Route as EditHotelImport } from "../frontend/src/pages/EditHotel";
+
+// const EditHotelIdRoute = EditHotelImport.update({
+//   path: "/edit-hotel/$hotelId",
+//   getParentRoute: () => rootRoute,
+// } as any);
+// declare module "@tanstack/react-router" {
+//   interface FileRoutesByPath {
+//     "/edit-hote/$hotelId": {
+//       preLoaderRoute: typeof EditHotelImport;
+//       parentRoute: typeof rootRoute;
+//     };
+//   }
+// }
+
+// export const routeTree = rootRoute.addChildren([EditHotelIdRoute]);
