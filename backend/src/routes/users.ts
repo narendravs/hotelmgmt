63f2a718831d37bbf21c8064a559470f22ca558e
narendravs <narendravs_mca@yahoo.com>
2025-04-17 17:19:52 +0530

    developed api's for the apllication

---
+++ backend/src/routes/users.ts
@@ -0,0 +1,74 @@
+import express, { Request, Response } from "express";
+import User from "../models/users";
+import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
+import verifyToken from "../middleware/auth";
+
+const router = express.Router();
+
+router.get(
+  "/getme",
+  verifyToken,
+  async (req: Request, res: Response): Promise<any> => {
+    const userId = req.userId;
+
+    try {
+      const user = await User.findOne({ _id: userId });
+
+      if (!user) {
+        return res.status(200).json({ message: "User not found" });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ message: "something went wrong" });
+    }
+  }
+);
+
+router.post(
+  "/register",
+  [
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response): Promise<any> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+
+    try {
+      const userExists = await User.findOne({ email: req.body.email });
+      if (userExists) {
+        return res.status(200).json({ message: "User alreday exists" });
+      }
+
+      const user = new User(req.body);
+
+      await user.save();
+
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string,
+        {
+          expiresIn: "1d",
+        }
+      );
+      res.cookie("auth_cookie", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 86400000,
+      });
+      return res.status(200).json({ message: "User registered OK" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+export default router;
