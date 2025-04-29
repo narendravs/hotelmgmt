b7a882e45bc2d5ac8d55faaa0f5c04714cbc96a2
narendravs <narendravs_mca@yahoo.com>
2025-04-29 18:57:39 +0530

    added footer compoenent

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-blue-800 py-10 mx-1 rounded-sm">
+      <div className=" container mx-auto flex  justify-between items-center">
+        <span className=" text-3xl text-white font-bold tracking-tight flex gap-4">
+          MernHolidays.com
+        </span>
+        <span>
+          <p className="cursor-pointer">Privacy Policy</p>
+          <p className="cursor-pointer">Terms of Service</p>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
