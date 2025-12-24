ad9b0592928c21c75ff552046e30f956b5c07152
narendravs <narendravs_mca@yahoo.com>
2025-12-24 11:39:11 +0530

    chore: remove dead code and unused imports

--- frontend/src/forms/ManageHotelForm/FacilitiesSection.tsx
+++ frontend/src/forms/ManageHotelForm/FacilitiesSection.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { useFormContext } from "react-hook-form";
 import { HotelFormData } from "./ManageHotelForm";
 import { hotelFacilities } from "../../config/hotel-options-config";
