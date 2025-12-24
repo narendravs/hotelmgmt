6369bb6ca56db412b13b042385b4146ef377f400
narendravs <narendravs_mca@yahoo.com>
2025-12-24 11:02:22 +0530

    Refactor: remove redundant useQuery and fix input bindings in hotel form sections

--- frontend/src/forms/ManageHotelForm/GuestsSection.tsx
+++ frontend/src/forms/ManageHotelForm/GuestsSection.tsx
@@ -1,46 +1,13 @@
 import { useFormContext } from "react-hook-form";
 import { HotelFormData } from "./ManageHotelForm";
-import { useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import * as apiClient from "../../api-client";
-import { useEffect, useState } from "react";
+
 const GuestsSection = () => {
-  const { hotelId } = useParams();
-
-  // const [adultCount, setadultCount] = useState<number | undefined>();
-  // const [childCount, setchildCount] = useState<number | undefined>();
-
   const {
     register,
-    setValue,
-    watch,
+
     formState: { errors },
   } = useFormContext<HotelFormData>();
 
-  const { data } = useQuery(
-    ["fetchHotelById", hotelId],
-    async () => await apiClient.fetchHotelById(hotelId!),
-    {
-      onSuccess: () => {
-        setValue("adultCount", data?.adultCount || parseInt("0"));
-        setValue("childCount", data?.childCount || parseInt("0"));
-      },
-      enabled: !!hotelId,
-    }
-  );
-
-  // useEffect(() => {
-  //   const fetchHotelById = async () => {
-  //     const response = await apiClient.fetchHotelById(hotelId);
-  //     setadultCount(response.adultCount);
-  //     setchildCount(response.childCount);
-  //   };
-  //   if (hotelId) {
-  //     fetchHotelById();
-  //   }
-  // }, [hotelId]);
-  const adultCount = watch("adultCount");
-  const childCount = watch("childCount");
   return (
     <div>
       <h2 className="text-2xl font-bold mb-3">Guests</h2>
@@ -50,7 +17,6 @@ const GuestsSection = () => {
           <input
             className="border rounded w-full py-2 px-3 font-normal "
             type="number"
-            value={adultCount}
             min={1}
             {...register("adultCount", {
               required: "This field is required",
@@ -68,7 +34,6 @@ const GuestsSection = () => {
           <input
             className="border rounded w-full py-2 px-3 font-normal"
             type="number"
-            value={childCount}
             min={0}
             {...register("childCount", {
               required: "This field is required",
