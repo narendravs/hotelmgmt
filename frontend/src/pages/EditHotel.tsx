8784203ee3c60396021eaf20a0a0cd90519791fb
narendravs <narendravs_mca@yahoo.com>
2025-12-24 11:08:51 +0530

    feat: add query invalidation to refresh hotel data after updating

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -1,14 +1,13 @@
-import React from "react";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
 
-
 const EditHotel = () => {
   const { hotelId } = useParams();
   const { showToast } = useAppContext();
+  const queryClient = useQueryClient();
   const { data: hotel } = useQuery(
     ["fetchHotelById"],
     () => apiClient.fetchHotelById(hotelId || ""),
@@ -19,6 +18,7 @@ const EditHotel = () => {
 
   const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
     onSuccess: () => {
+      queryClient.invalidateQueries(["fetchHotelById", hotelId]);
       showToast({ message: "Error Saving Hotel", type: "SUCCESS" });
     },
     onError: () => {
