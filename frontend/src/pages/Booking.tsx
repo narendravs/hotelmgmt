8f422f8ba4b76fd9beb34aa2732fca7030355add
narendravs <narendravs_mca@yahoo.com>
2025-12-24 15:17:23 +0530

    refactor: remove unused imports and unused code

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useAppContext } from "../contexts/AppContext";
 import { useQuery } from "@tanstack/react-query";
 import * as apiClient from "../api-client";
